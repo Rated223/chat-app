79c9567eb3c64c958cac43704daadfd0be92ad56
Daniel Carrizales <rated223@outlook.com>
2019-09-29 16:49:34 -0500

    Adding the socket.io library
    Signed-off-by: Daniel Carrizales <rated223@outlook.com>

--- src/index.js
+++ src/index.js
@@ -1,7 +1,20 @@
-import app from './app';
+import path from "path";
+import http from "http";
+import  socketio from "socket.io";
+import { app, express } from './app';
 
-const port = process.env.PORT;
+const server = http.createServer(app);
+const io = socketio(server);
 
-app.listen(port, () => {
+const port = process.env.PORT || 3000;
+const publicDirectory = path.join(__dirname, '../public');
+
+app.use(express.static(publicDirectory));
+
+io.on('connection', () => {
+    console.log('new connection!');
+});
+
+server.listen(port, () => {
     console.log('Server up and running in port '+port);
-})
+});
